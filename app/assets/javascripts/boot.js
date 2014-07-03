f55114b47f88f3d5c95ba68ec1b8850381dfcead
filipecrosk <filipe@primeiroestilo.com.br>
2014-07-03 09:15:13 -0300

    configure dispatcher

---
+++ app/assets/javascripts/boot.js
@@ -0,0 +1,3 @@
+$(function(){
+  Dispatcher.init(MyApp);
+});
---
+++ app/assets/javascripts/users_controller.js
@@ -0,0 +1,17 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+
+var MyApp = {};
+
+MyApp.site = {
+  index: function(){
+    alert('Abriu a index de users');
+  },
+  new: function(){
+    alert('Abriu a new de users');
+  },
+  show: function(){
+    alert('Abriu a show de users');
+  }
+};
